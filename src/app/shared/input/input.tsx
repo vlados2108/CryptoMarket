9b2bef13a733e621beb3a4a8124fcf19e9d803fc
vlados2108 <verlov.vladislav@mail.ru>
2023-09-26 23:39:47 +0300

    created reusable input component

---
+++ src/app/shared/input/input.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import styles from './css/input.module.css'
+type InputProps = {
+    handler?: (value: string) => void
+    className?: string
+    placeholder?: string
+    customAttrs?: Record<string, string>
+    type?: string
+}
+export default function Input({
+    handler,
+    className,
+    placeholder,
+    customAttrs,
+    type,
+}: InputProps) {
+    const [value, setValue] = useState('')
+
+    return (
+        <input
+            placeholder={placeholder}
+            className={classNames({
+                [styles.input]: true,
+                [className as string]: !!className,
+            })}
+            {...customAttrs}
+            value={value}
+            type={type}
+            onChange={(e) => {
+                setValue(e.target.value)
+                handler ? handler(e.target.value) : ''
+            }}
+        ></input>
+    )
+}
