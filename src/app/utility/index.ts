326a6cdf1cb2cfd54758d6a8909755c2f452771f
vlados2108 <verlov.vladislav@mail.ru>
2023-09-27 19:36:58 +0300

    added functions for sorting and filtering

--- src/app/utility/index.ts
+++ src/app/utility/index.ts
@@ -1,3 +1,5 @@
+import { Coin, Filters } from '../types'
+
 export const formatPrice = (price: number): string => {
     if (Math.abs(price) >= 1e9) {
         return (price / 1e9).toFixed(2) + 'b'
@@ -10,6 +12,73 @@ export const formatPrice = (price: number): string => {
     }
 }
 
-export const getLogoUrl = (coinSymbol: string):string => {
+export const getLogoUrl = (coinSymbol: string): string => {
     return `https://assets.coincap.io/assets/icons/${coinSymbol.toLowerCase()}@2x.png`
+}
+
+// Функция сортировки по возрастанию
+export function sortByAscending(coins: Coin[], key: keyof Coin) {
+    //return [...coins].sort((a, b) => a[key] - b[key]);
+    return [...coins].sort((a, b) => {
+        if (a[key] < b[key]) return -1
+        if (a[key] > b[key]) return 1
+        return 0
+    })
+}
+
+// Функция сортировки по убыванию
+export function sortByDescending(coins: Coin[], key: keyof Coin) {
+    return [...coins].sort((a, b) => {
+        if (a[key] > b[key]) return -1
+        if (a[key] < b[key]) return 1
+        return 0
+    })
+}
+
+export function filterCoins(coins: Coin[], filters: Filters) {
+    return coins.filter((coin) => {
+        // Функция проверки диапазона
+        const isInRange = (
+            value: number,
+            min: number | undefined,
+            max: number | undefined
+        ) => {
+            if (
+                min !== undefined &&
+                min.toString() !== '' &&
+                max !== undefined &&
+                max.toString() !== ''
+            ) {
+                return value >= min && value <= max
+            } else if (min !== undefined && min.toString() !== '') {
+                return value >= min
+            } else if (max !== undefined && max.toString() !== '') {
+                return value <= max
+            }
+            return true
+        }
+
+        // Фильтрация по цене
+        const isPriceInRange = isInRange(
+            coin.priceUsd,
+            filters.price1,
+            filters.price2
+        )
+
+        // Фильтрация по капитализации
+        const isCapInRange = isInRange(
+            coin.marketCapUsd,
+            filters.cap1,
+            filters.cap2
+        )
+
+        // Фильтрация по процентному изменению
+        const isPercInRange = isInRange(
+            coin.changePercent24Hr,
+            filters.perc1,
+            filters.perc2
+        )
+
+        return isPriceInRange && isCapInRange && isPercInRange
+    })
 }
