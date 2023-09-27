0c0127eff23accdcee048750ce44b5e24d490ac8
vlados2108 <verlov.vladislav@mail.ru>
2023-09-27 12:55:28 +0300

    created modal component

--- src/app/shared/modal/modal.tsx
+++ src/app/shared/modal/modal.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
-export default function Modal() {
-  return (
-    <div>M</div>
-  )
+import React, { Dispatch, ReactNode, SetStateAction } from 'react'
+import "./modal.module.scss"
+interface ActiveProps {
+    active: boolean
+    setActive: Dispatch<SetStateAction<boolean>>
+    children: ReactNode
+}
+export default function Modal(props: ActiveProps) {
+    return (
+        <div
+            className={props.active ? 'modal active' : 'modal'}
+            onClick={() => {
+                props.setActive(false)
+            }}
+        >
+            <div
+                className={
+                    props.active ? 'modal-content active' : 'modal-content'
+                }
+                onClick={(e) => e.stopPropagation()}
+            >
+                {props.children}
+            </div>
+        </div>
+    )
 }
