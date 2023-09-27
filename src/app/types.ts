3c0a91aa888a0808742a6f09e8e305af5c500be2
vlados2108 <verlov.vladislav@mail.ru>
2023-09-27 19:38:42 +0300

    added filters type

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,24 +1,33 @@
-export interface CoinResponse{
-    id: string,
-    rank: string,
-    symbol: string,
-    name: string,
-    supply: string,
-    maxSupply: string,
-    marketCapUsd: string,
-    volumeUsd24Hr: string,
-    priceUsd: string,
-    changePercent24Hr: string,
-    vwap24Hr: string,
+export interface CoinResponse {
+    id: string
+    rank: string
+    symbol: string
+    name: string
+    supply: string
+    maxSupply: string
+    marketCapUsd: string
+    volumeUsd24Hr: string
+    priceUsd: string
+    changePercent24Hr: string
+    vwap24Hr: string
     explorer: string
 }
 
-export interface Coin{
-    id:string,
-    symbol:string,
-    name:string,
-    priceUsd:number,
-    marketCapUsd:number,
-    changePercent24Hr:number,
-    logoUrl:string,
+export interface Coin {
+    id: string
+    symbol: string
+    name: string
+    priceUsd: number
+    marketCapUsd: number
+    changePercent24Hr: number
+    logoUrl: string
+}
+
+export interface Filters {
+    price1: number | undefined
+    price2: number | undefined
+    cap1: number | undefined
+    cap2: number | undefined
+    perc1: number | undefined
+    perc2: number | undefined
 }
