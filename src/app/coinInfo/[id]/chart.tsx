95ab29b7fc12a16a47d55909ee4174c5d686678d
vlados2108 <verlov.vladislav@mail.ru>
2023-09-28 19:58:12 +0300

    updated chart component to be responsive to parent

--- src/app/coinInfo/[id]/chart.tsx
+++ src/app/coinInfo/[id]/chart.tsx
@@ -12,47 +12,47 @@ import { coinPrice } from './types'
 
 interface IChartProps {
     graphData: coinPrice[]
-    width:number,
-    height:number
+    width: number
+    height: number
 }
 
-export default function Chart({ graphData,width,height }: IChartProps) {
+export default function Chart({ graphData, width, height }: IChartProps) {
     return (
-        <AreaChart
-            width={width}
-            height={height}
-            data={graphData}
-            margin={{
-                top: 10,
-                right: 0,
-                left: 20,
-                bottom: 0,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-                dataKey="Date"
-                label={{
-                    value: 'Date',
-                    position: 'insideBottomRight',
-                    offset: 0,
+        <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+                data={graphData}
+                margin={{
+                    top: 10,
+                    right: 20,
+                    left: 20,
+                    bottom: 0,
                 }}
-            />
-            <YAxis
-                label={{
-                    value: 'Prise(USD)',
-                    position: 'insideLeft',
-                    angle: -90,
-                    offset: 0,
-                }}
-            />
-            <Tooltip />
-            <Area
-                type="monotone"
-                dataKey="Price"
-                stroke="#8884d8"
-                fill="#8884d8"
-            />
-        </AreaChart>
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                    dataKey="Date"
+                    label={{
+                        value: 'Date',
+                        position: 'insideBottomRight',
+                        offset: 0,
+                    }}
+                />
+                <YAxis
+                    label={{
+                        value: 'Prise(USD)',
+                        position: 'insideLeft',
+                        angle: -90,
+                        offset: 0,
+                    }}
+                />
+                <Tooltip />
+                <Area
+                    type="monotone"
+                    dataKey="Price"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                />
+            </AreaChart>
+        </ResponsiveContainer>
     )
 }
