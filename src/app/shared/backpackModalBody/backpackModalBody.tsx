0974f8e762f0d457d1d2765a199679edbc9305dc
vlados2108 <verlov.vladislav@mail.ru>
2023-09-30 01:10:41 +0300

    created component for backpack

--- src/app/shared/backpackModalBody/backpackModalBody.tsx
+++ src/app/shared/backpackModalBody/backpackModalBody.tsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles from './backpackModalBody.module.scss'
+import { appPrefix, formatNumber } from '@/app/shared/utility'
+import { coinInBackPack } from './types'
+import Button from '../button'
+import { useGlobalContext } from '@/app/context'
 
 export default function BackpackModalBody() {
-  return (
-    <div>BbackpackModalBody</div>
-  )
+    const [coins, setCoins] = useState<coinInBackPack[]>([])
+    const {dateAdded} = useGlobalContext()
+    useEffect(() => {
+        const coins: coinInBackPack[] = []
+        for (let key in localStorage) {
+            if (!localStorage.hasOwnProperty(key)) {
+                continue
+            }
+            if (key.startsWith(appPrefix)) {
+                const data: coinInBackPack = JSON.parse(localStorage[key])
+                coins.push(data)
+            }
+        }
+        setCoins(coins)
+    }, [dateAdded])
+
+    const deleteCoin = (id: string) => {
+        localStorage.removeItem(appPrefix + id)
+        const newCoins = coins.filter((coin)=>coin.id!=id)
+        setCoins(newCoins)
+    }
+
+    return (
+        <div className={styles['backpack-container']}>
+            <div className={styles['backpack-header']}>Backpack</div>
+            <div className={styles['backpack-body']}>
+                <div
+                    className={`${styles['backpack-column']} ${styles['header']}`}
+                >
+                    Name
+                </div>
+                <div
+                    className={`${styles['backpack-column']} ${styles['header']}`}
+                >
+                    Count
+                </div>
+                <div
+                    className={`${styles['backpack-column']} ${styles['header']}`}
+                >
+                    Total Price
+                </div>
+                <div
+                    className={`${styles['backpack-column']} ${styles['header']}`}
+                >
+                    Delete
+                </div>
+                {coins.map((coin) => {
+                    return (
+                        <>
+                            <div className={styles['backpack-column']}>
+                                {coin.name}
+                            </div>
+                            <div className={styles['backpack-column']}>
+                                {coin.count}
+                            </div>
+                            <div className={styles['backpack-column']}>
+                                {formatNumber(coin.totalPrice)} USD
+                            </div>
+                            <div className={styles['backpack-column']}>
+                                <Button
+                                    value="Delete"
+                                    className={styles['backpack-remove-btn']}
+                                    handler={() => {
+                                        deleteCoin(coin.id)
+                                    }}
+                                />
+                            </div>
+                        </>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
