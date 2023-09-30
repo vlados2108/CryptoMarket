7cc1ba2370c8dbdb2be40519f24882fa4e6d4453
vlados2108 <verlov.vladislav@mail.ru>
2023-09-30 14:53:18 +0300

    fixed handler parameter

--- src/app/shared/button/button.tsx
+++ src/app/shared/button/button.tsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import classNames from 'classnames'
 import styles from './button.module.scss'
 type InputProps = {
-    handler?: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    handler?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
     className?: string
     customAttrs?: Record<string, string>
     value?: string
     key?: number
 }
-export default function button({
-    handler,
+export default function Button({
+    handler = undefined,
     className,
     customAttrs,
     value,
@@ -22,10 +22,10 @@ export default function button({
                 [styles.button]: true,
                 [className as string]: !!className,
             })}
-            {...customAttrs}
             onClick={(e) => {
-                handler ? handler(e) : ''
+                handler ? handler(e) : undefined
             }}
+            {...customAttrs}
         >
             {value}
         </button>
